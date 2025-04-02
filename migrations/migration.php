d4ef1068397ff61c9b2eaf84935ec2325f10257d
Bobomurod <bobomurodamirov.b@gmail.com>
2025-04-02 21:02:59 +0500

    Add task.php

--- migrations/migration.php
+++ migrations/migration.php
@@ -34,12 +34,12 @@ try{
     $db->exec("CREATE TABLE IF NOT EXISTS requirements(
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(128),
-    resource VARCHAR(128),
+    resource VARCHAR(128),$this->cur
     task_id INT,
     FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE
     )");
 
-    $db->exec("CREATE TABLE required_knowledge(
+    $db->exec("CREATE TABLE IF NOT EXISTS required_knowledge(
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(128),
     resource VARCHAR(128),
