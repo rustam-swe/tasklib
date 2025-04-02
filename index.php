18a267c3d792dfb5e4d03ff6189bb27d2976b094
XR-Dante <telegram77012@gmail.com>
2025-04-02 17:49:29 +0500

    Added form adminPage

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 declare(strict_types=1);
 echo 'Task Library';
+
+//require_once __DIR__ . '/Home_page/homePage.php';
+?>
+
+<html>
+<head>
+</head>
+
+<body>
+<div id="center_button">
+    <button onclick="location.href='/Home_page/homePage.php'">Home Page</button>
+</div>
+</body>
+</html>
