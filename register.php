ef825fe05af3087c2eb0c58e3a6ebc63d75e94f2
Lazizjon308 <you@example.com>
2025-04-02 18:40:12 +0500

    Initial commit

---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign up</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form>
+        <h2>Sign up</h2>
+        <input type="text" placeholder="Enter your name" required><br><br>
+        <input type="text" placeholder="Create password" required><br><br>
+        <input type="text" placeholder="Confirm password" required><br><br>
+        <button type="submit">Sign up</button>
+    </form>
+</body>
