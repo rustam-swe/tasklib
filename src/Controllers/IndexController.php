2be923aff5d5d8175931bc780513e8228c70e4dd
afifao1 <afifao1>
2025-04-04 20:43:20 +0500

    MAJOR: migration writes to a separate file

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -3,16 +3,10 @@
 declare(strict_types=1);
 
 namespace App\Controllers;
-<<<<<<< HEAD
-// use \App\Models\UserModel;
-class IndexController {
-  public function index(){
-    // var_dump((new UserModel())->getUserById(1));
-=======
+
 
 class IndexController {
   public function index(){
->>>>>>> 11b45d1 (PATCH: Fixed router)
     echo 'task library';
   }
 public function home(){
