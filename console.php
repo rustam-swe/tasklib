694b9ad3b417a8f27dfb904b8aefd8fbbc2bc789
Rustam <rustamergashev.sp@gmail.com>
2025-04-09 20:12:31 +0500

    PATCH: handled unmchatched branch in console.php

--- console.php
+++ console.php
@@ -3,8 +3,33 @@ declare(strict_types=1);
 
 require "Core/Database/Migration.php";
 
+$commands = [
+  'migrate'
+];
+
+if(!isset($argv[1])){
+  $errorText ='Argument kiritish talab etiladi' . PHP_EOL;
+  $errorText .= "Mana bu buyruqlardan birini kiritish mumkin: ". PHP_EOL;
+ 
+  echo $errorText . implode(PHP_EOL, $commands) . PHP_EOL;
+
+  return;
+}
+
 $command = $argv[1];
 
-match($command) {
-  'migrate' => \Core\Database\Migration::migrate(__DIR__)
+$argumentMissingError = function(string $command) use ($commands) {
+  if(!in_array($command, $commands)){
+    $errorText = "$command buyrug'i mavjud emas." . PHP_EOL;
+    $errorText .= "Mana bu buyruqlardan birini kiritish mumkin: ". PHP_EOL;
+    
+    echo $errorText . implode(PHP_EOL, $commands) . PHP_EOL;
+  }
 };
+
+match($command) {
+  'migrate' => \Core\Database\Migration::migrate(__DIR__),
+  default => $argumentMissingError($command)
+};
+
+
