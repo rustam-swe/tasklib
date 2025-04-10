471de5d2177d9c269d71fa2b1435955810b93323
Bobomurod <bobomurodamirov.b@gmail.com>
2025-04-10 17:47:15 +0500

    Add task.php

--- resource/views/task.php
+++ resource/views/task.php
@@ -8,13 +8,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <style>
       body{
-<<<<<<< HEAD
-        background-color: gray;
-=======
         background-color: #000000;
->>>>>>> 2d9edca (PATCH: css is changed in task.php)
       }
       .delete{
+        width: 100%;
+        height: 50px;
+      }
+      .start{
         width: 100%;
         height: 50px;
       }
@@ -24,11 +24,8 @@
       }
       .task{
         margin-top: 60px;
-<<<<<<< HEAD
         background-color: #fff;
-=======
         background-color: #F0F8FF;
->>>>>>> 2d9edca (PATCH: css is changed in task.php)
         border-radius: 13px;
         padding: 50px;
       }
@@ -57,44 +54,98 @@
     <div class="container task">
         <div class="row">
             <div class="col-10">
-            <h1><?=$task['title']?></h1>
-            <p class="desc fs-3"><?=$task['description']?></p>
+            <h1>TASK</h1>
+            <p class="desc fs-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab eveniet nihil cum nam, deserunt quia magnam amet? Sequi rerum assumenda nulla eos optio labore vero, ipsam sapiente quibusdam, facere fuga?Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aliquam voluptatem, unde nam commodi magnam impedit laborum, non asperiores officiis est repudiandae provident aut amet! Quo sit exercitationem distinctio at. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic consequuntur ipsum, aperiam excepturi officia placeat repellendus amet ut. Sint culpa ad perferendis molestiae, hic qui rerum corrupti. Quos, eaque repellat.</p>
                 <div style="display:flex; justify-content:space-between;">
                   <div>
                   <h2 class="req">Requirements:</h2>
                 <ul class="text-secondary fs-5">
-<?php foreach($task['requirements'] as $requirement):?>  
 <li style="font-size: 1.5em; color: black; text-primary">
-<a class="req_a" href="<?=$requirement['resource']?>"><?=$requirement['title']?></a>
+<a class="req_a" href="https://www.php.net/" target="_blank">PHP</a>
 </li>
-<?php endforeach;?>
                   </ul>
                   </div>
                   <div>
                   <h2 class="req">Required knowledge:</h2>
                 <ul class="text-secondary fs-5">
-                  <?php foreach($task['requiredKnowledges'] as $requirement):?>
 <li style="font-size: 1.5em; color: black; text-primary">
-<a class="req_a" href="<?=$requirement['resource']?>"><?=$requirement['title']?></a>
+<a class="req_a" href="https://www.php.net/" target="_blank">PHP</a>
 </li>
-<?php endforeach;?>
                 </ul>
                   </div>
                 </div>
             
             </div>
             <div class="col">
-              <a href="" class="btn btn-danger delete"><h4 align="center">Delete</h4></a>
+              <a href="" class="btn btn-info start"><h4 align="center">Start</h4></a>
+              <a href="" class="btn btn-danger delete mt-3"><h4 align="center">Delete</h4></a>
               <a href="" class="btn btn-warning edit mt-3"><h4 align="center">Edit</h4></a>
               <h4 class="mt-5 fst-italic">difficulty level:</h4>
-              <h3 align="center" class="mt-1 border border-3 border-dark text-danger"><?=$task['difficulty']?></h3>
-              <h3 align="center" class="mt-1 border border-3 border-dark text-warning">start button</h3>
+              <h3 align="center" class="mt-1 border border-3 border-dark text-warning">in progress</h3>
               <h4 class="mt-4 fst-italic">deadline:</h4>
               <i class="bi bi-hourglass text-danger fs-3"></i>
-              <span class="ms-2 fw-bold text-secondary fs-4"><?=$task['deadline']?></span>
+              <span class="ms-2 fw-bold text-secondary fs-4" id="deadline">24:00:00</span>
             </div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script>
+      const deadlineEl = document.getElementById('deadline');
+      let timerStarted = false;
+      let timerInterval;
+
+      function updateTimerDisplay(totalSeconds) {
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+        const seconds = totalSeconds % 60;
+      
+        deadlineEl.textContent =
+          String(hours).padStart(2, '0') + ':' +
+          String(minutes).padStart(2, '0') + ':' +
+          String(seconds).padStart(2, '0');
+      }
+    
+      function startCountdown(savedTime = null) {
+        timerStarted = true;
+        let endTime;
+      
+        if (savedTime) {
+          endTime = new Date(savedTime);
+        } else {
+          endTime = new Date(Date.now() + 24 * 60 * 60 * 1000);
+          localStorage.setItem('deadline', endTime);
+        }
+      
+        function tick() {
+          const now = new Date();
+          const totalSeconds = Math.floor((new Date(endTime) - now) / 1000);
+        
+          if (totalSeconds > 0) {
+            updateTimerDisplay(totalSeconds);
+          } else {
+            clearInterval(timerInterval);
+            deadlineEl.textContent = '00:00:00';
+            localStorage.removeItem('deadline');
+          }
+        }
+      
+        tick();
+        timerInterval = setInterval(tick, 1000);
+      }
+    
+      document.querySelector('.start').addEventListener('click', function (event) {
+        event.preventDefault();
+        if (timerStarted) return;
+      
+        startCountdown();
+      });
+    
+      const savedDeadline = localStorage.getItem('deadline');
+      if (savedDeadline) {
+        startCountdown(savedDeadline);
+      }
+    </script>
+
+
   </body>
 </html>
